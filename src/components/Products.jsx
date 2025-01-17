2be6db13c61b5bff24bd7c1c64d1fce22181e27a
Temurbek <temurbekgulboyev01.com>
2025-01-17 13:28:23 +0500

    first commit7

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import useProducts from '../hooks/useProducts'
+import useProducts from '../hooks/UseProducts'
 import PlaseHolderCards from './plaseHolderCards'
 import Productcards from './Productcards'
 
