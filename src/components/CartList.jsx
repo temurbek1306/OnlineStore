7e263b22f7085cb86dff3b7837befa688fab103c
Temurbek <temurbekgulboyev01.com>
2025-01-26 20:13:27 +0500

    new add files

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
-import { decItemCount, incItemCount, removeItemFromCart } from '../ReduxToolkit/Store/slices/cart'
+import { decItemCount, incItemCount, removeItemFromCart } from  '../ReduxToolkit/store/slices/cart'
 
 function CartList() {
 
